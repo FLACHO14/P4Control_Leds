76058d6d9c10af95eafaffeca26ae26ac7f645a3
FLACHO14 <168665710+FLACHO14@users.noreply.github.com>
2024-06-15 16:14:46 -0500

    Update pulsantes.h
    se Añadió el sistema anti rebote

--- src/pulsantes.h
+++ src/pulsantes.h
@@ -4,7 +4,32 @@
 
 #include <stdint.h>
 #include "hal.h"
+#include <stdbool.h>
+
+///////// add del sistema anti rebote////
+
+
+typedef enum {
+    BUTTON_UP,
+    BUTTON_FALLING,
+    BUTTON_DOWN,
+    BUTTON_RISING
+} ButtonState;
+
+typedef struct {
+    ButtonState state;
+    int16_t pin;
+    uint32_t lastDebounceTime;
+    uint32_t debounceDelay;
+} DebounceButton;
 
 bool leerBot(int16_t bot);
+void initButton(DebounceButton *button, int16_t pin, uint32_t debounceDelay);
+void updateButton(DebounceButton *button)
+
+
+
+
+
 
 #endif
