232634ed8a88587219a27ebea2353da2059bf073
FLACHO14 <168665710+FLACHO14@users.noreply.github.com>
2024-06-30 14:08:11 -0500

    Update pulsantes.h

--- src/pulsantes.h
+++ src/pulsantes.h
@@ -1,35 +1,22 @@
-#ifndef __PULSANTES_H
-#define __PULSANTES_H
+#ifndef PULSANTES_H
+#define PULSANTES_H
 
-
-#include <stdint.h>
-#include "hal.h"
-#include <stdbool.h>
-
-///////// add del sistema anti rebote/////
-
-
-typedef enum {
-    BUTTON_UP,
-    BUTTON_FALLING,
-    BUTTON_DOWN,
-    BUTTON_RISING
-} ButtonState;
+#include <Arduino.h>
 
 typedef struct {
-    ButtonState state;
-    int16_t pin;
-    uint32_t lastDebounceTime;
-    uint32_t debounceDelay;
+    int pin;
+    int state;
+    int lastState;
+    unsigned long lastDebounceTime;
+    unsigned long debounceDelay;
 } DebounceButton;
 
+void init_pulsadores();
+void update_pulsadores();
 bool leerBot(int16_t bot);
 void initButton(DebounceButton *button, int16_t pin, uint32_t debounceDelay);
-void updateButton(DebounceButton *button)
-
-
-
-
-
+void updateButton(DebounceButton *button);
+void buttonPressedEvent();
+void buttonReleasedEvent();
 
 #endif
